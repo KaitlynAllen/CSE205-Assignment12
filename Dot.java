8b1855e505ca36ded9653c84cd1f7008d1611beb
Kaitlyn Allen <kaitlynpa99@gmail.com>
2018-04-27 06:54:11 -0700

    Adding header

--- Dot.java
+++ Dot.java
@@ -1,3 +1,9 @@
+// Assignment #: 12
+//         Name: Kaitlyn Allen
+//    StudentID:
+//      Lecture: TTH 4:30-5:45
+//  Description: The Dot class creates a dot/circle that makes up
+//               shape of the wave.
 import java.awt.Color;
 import java.awt.Graphics;
 
